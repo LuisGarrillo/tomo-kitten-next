d857319161900c7499caa549d8c392b3d3eda580
Luis Garrillo <lggarrillo@urbe.edu.ve>
2025-03-23 22:58:33 -0400

    Update customize page

--- src/app/customize/page.js
+++ src/app/customize/page.js
@@ -1,7 +1,14 @@
+import TimerLayout from "../ui/components/layout";
+import Kitten from "../ui/components/kitten";
+
 export default function Customize() {
     return (
       <div>
-        Customize
+        <TimerLayout>
+          <Kitten/>
+          <div>
+          </div>
+        </TimerLayout>
       </div>
     );
   }
