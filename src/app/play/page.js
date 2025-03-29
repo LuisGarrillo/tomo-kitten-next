0a1681b89586b1f03cc37969c66afb23fd5204f7
Luis Garrillo <lggarrillo@urbe.edu.ve>
2025-03-29 13:23:56 -0400

    Implement base button

--- src/app/play/page.js
+++ src/app/play/page.js
@@ -4,18 +4,27 @@ import Kitten from "../ui/components/kitten";
 import moodChange from "../events/moodChange";
 import TimerLayout from "../ui/components/layout";
 import { statsData } from "../ui/components/timer";
+import BaseButton from "../ui/components/baseButton";
 
 export default function Play() {
-    function raiseMood(e) {
-      moodChange.emit("moodRaised", e.target.value);
+    function raiseMood(value) {
+      moodChange.emit("moodRaised", value);
     }
 
     return (
       <TimerLayout>
         <Kitten/>
-        <div>
-          <button onClick={raiseMood} value={"hygiene"}>Peinar</button>
-          <button onClick={raiseMood} value={"happiness"}>Acariciar</button>
+        <div className="button-wrapper">
+          <BaseButton
+            event={raiseMood}
+            text={"Limpiar"}
+            value={"hygiene"}
+          />
+          <BaseButton
+            event={raiseMood}
+            text={"Acariciar"}
+            value={"happiness"}
+          />
         </div>
       
       </TimerLayout>
