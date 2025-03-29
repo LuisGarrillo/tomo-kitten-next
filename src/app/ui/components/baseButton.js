2db0e3fe896f7a5a22c7cf03d8491e09c1aeb70b
Luis Garrillo <lggarrillo@urbe.edu.ve>
2025-03-29 13:23:45 -0400

    Create base button component

---
+++ src/app/ui/components/baseButton.js
@@ -0,0 +1,30 @@
+'use client'
+import Image from "next/image"
+import { useEffect, useState } from "react";
+import moodChange from "@/app/events/moodChange";
+import Link from "next/link";
+
+
+
+export default function BaseButton({event, text, value}) {
+    let given_listener = event;
+    function select() {
+        given_listener(value);
+    }
+
+    return (
+        <div className="base-button">
+            <Image
+                src={`/buttons/empty/botones_vacios.png`}
+                width={350}
+                height={100}
+                alt="button"
+                onClick={select}
+            >
+            </Image>
+            <p>
+                {text}
+            </p>
+        </div>  
+    );
+}
