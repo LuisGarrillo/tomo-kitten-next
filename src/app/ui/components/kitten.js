53724e6eab80c523bd378ea93f07383a8d3cf92d
Luis Garrillo <lggarrillo@urbe.edu.ve>
2025-03-30 21:15:31 -0400

    Add accessory to kitten component

--- src/app/ui/components/kitten.js
+++ src/app/ui/components/kitten.js
@@ -8,32 +8,52 @@ import { catData } from './timer';
 
 export default function Kitten() {
   let [mood, setMood] = useState(catData["mood"]);
+  let [accesory, setAccesory] = useState(catData["accesory"]);
   let [name, setName] = useState("");
+  
+  useEffect(() => {
+
+  }, []);
+
   useEffect(() => {
     mood = catData["mood"];
     setMood(mood);
     setName(catData["name"]);
+    setAccesory(catData["accessory"]);
     
     const handleMoodChange = (receivedMood) => {
-        setMood(receivedMood);
-        mood = receivedMood;
+      setMood(receivedMood);
+      mood = receivedMood;
+    };
+
+    const handleAccesoryChange = (receivedAccesory) => {
+      setAccesory(receivedAccesory);
     };
 
     moodChange.on('moodChanged', handleMoodChange);
+    moodChange.on('accesoryChanged', handleAccesoryChange);
 
     return () => {
         moodChange.off('moodChanged', handleMoodChange);
+        moodChange.off('accesoryChanged', handleAccesoryChange);
     };
   }, []);
 
   return (
     <div className='kitten-wrapper'>
         <Image
+          className='accesory'
+          src={`/clothes/${accesory}.png`}
+          width={370}
+          height={190}
+          alt="Your kitten accesory"
+        />
+        <Image
           src={`/kitten/${mood}.gif`}
           width={370}
           height={370}
           alt="Your kitten"
-      />
+        />
       <p>
         {name}
       </p>
