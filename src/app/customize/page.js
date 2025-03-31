67bb799e6ba6afd495581f3899a8b3d90c44b1e2
Luis Garrillo <lggarrillo@urbe.edu.ve>
2025-03-30 21:16:21 -0400

    Add customize functionality

--- src/app/customize/page.js
+++ src/app/customize/page.js
@@ -2,28 +2,69 @@
 
 import TimerLayout from "../ui/components/layout";
 import Kitten from "../ui/components/kitten";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { catData } from "../ui/components/timer";
+import ImageButton from "../ui/components/imageButton";
+import moodChange from "../events/moodChange";
+import { base } from "../ui/defaults/defaultPage";
 
 export default function Customize() {
     const [selectedAccessory, setSelectedAccessory] = useState(catData.accessory);
+    let [page, setPage] = useState(base);
 
-    function selectAccessory(accessory) {
-        setSelectedAccessory(accessory);
-        catData.accessory = accessory;  // Se guarda en los datos actuales del gato
-    }
-
-    return (
+    const customize = (
         <TimerLayout>
             <div className="page-wrapper">
                 <Kitten accessory={selectedAccessory} />
                 <div className="button-wrapper">
-                    <button onClick={() => selectAccessory("accessory1")}>Accesorio 1</button>
-                    <button onClick={() => selectAccessory("accessory2")}>Accesorio 2</button>
-                    <button onClick={() => selectAccessory("accessory3")}>Accesorio 3</button>
-                    <button onClick={() => selectAccessory("accessory4")}>Accesorio 4</button>
+                    <ImageButton
+                        source={"/clothes-icons/cap.png"}
+                        width={105}
+                        height={40}
+                        listener={selectAccessory}
+                        data={"cap"}
+                    />
+                    <ImageButton
+                        source={"/clothes-icons/glasses.png"}
+                        width={105}
+                        height={40}
+                        listener={selectAccessory}
+                        data={"glasses"}
+                    />
+                    <ImageButton
+                        source={"/clothes-icons/ribbon.png"}
+                        width={105}
+                        height={40}
+                        listener={selectAccessory}
+                        data={"ribbon"}
+                    />
+                    <ImageButton
+                        source={"/clothes-icons/leaf.png"}
+                        width={105}
+                        height={40}
+                        listener={selectAccessory}
+                        data={"leaf"}
+                    />
+                    <ImageButton
+                        source={"/clothes-icons/hcap.png"}
+                        width={105}
+                        height={40}
+                        listener={selectAccessory}
+                        data={"hcap"}
+                    />
                 </div>
             </div>
         </TimerLayout>
     );
+
+    function selectAccessory(accessory) {
+        moodChange.emit("accesoryChanged", accessory);
+        catData.accessory = accessory;
+    }
+    useEffect(() => {
+        setPage(customize);
+        moodChange.emit("loaded", "customize");
+    }, [])
+
+    return page;
 }
