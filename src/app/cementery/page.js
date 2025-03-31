0a7f24be1ea6425f7970e297dca98ea1a512fa1f
Luis Garrillo <lggarrillo@urbe.edu.ve>
2025-03-30 21:17:47 -0400

    Implement functionality to graveyard page

--- src/app/cementery/page.js
+++ src/app/cementery/page.js
@@ -1,35 +1,48 @@
 'use client'
 import { useState, useEffect } from "react";
 import { getData } from "../cache";
-const base = (
-  <div></div>
-)
+import { base } from "../ui/defaults/defaultPage";
+import MiniKitten from "../ui/components/miniKitten";
+import moodChange from "../events/moodChange";
 
 export default function Cementery() {
     let [page, setPage] = useState(base);
 
     function getHistory() {
       let history = getData("history", {"mascots": []});
+
       if (!history)
         return (
           <p>Parece que no has perdido a ninguna mascota, tu consciencia debe estar limpia!</p>
         );
-      console.log(history);
-      const elements = history["mascots"].map((mascot) => (
-        <div className="cementery-entry" key={mascot}>
+
+      let counter = 0;
+      const elements = history["mascots"].map((mascot) => {
+        counter++;
+
+        let accesory = (mascot["accessory"]) ? mascot["accessory"] : "none";
+        let name = (mascot["name"]) ? mascot["name"] : "Mascota sin nombre";
+
+        return (
+        <div className="cementery-entry" key={counter}>
+          <MiniKitten accesory={accesory} />
           <p>
-            {mascot["name"]}
+            {name}
           </p>
         </div>
-      ));
+      )});
+
       return elements;
     }
     const history = getHistory();
 
     const cementery = (
-      <div>
-        <h1>Bienvenido al cementerio, aqui puedes ver las consecueincias de tus acciones</h1>
-        <div>
+      <div className="cementery-page">
+        <div className="cementery-header">
+          <h1>Cementerio</h1>
+          <p>Aqui reposan nuestros viejos companeros.</p>
+        </div>
+        <div className="cementery-wrapper">
           {history}
         </div>
       </div>
@@ -38,6 +51,7 @@ export default function Cementery() {
 
     useEffect(() => {
       setPage(cementery);
+      moodChange.emit("loaded", "cementery");
     }, [])
 
     return page;
